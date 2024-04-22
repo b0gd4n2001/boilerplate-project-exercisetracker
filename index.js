bc54d47bdb4a7f59a863056a96588bdb91874162
b0gd4n2001 <lulelarub@gmail.com>
2024-04-22 22:21:43 +0300

    feat: posting username creates username document

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb+srv://bogdan:2Ff2NfVvK42ZbWw4@cluster0.qjyufua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+const usernameSchema = new mongoose.Schema({username: String});
+const usernameModel = mongoose.model('username', usernameSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +15,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', bodyParser.urlencoded({extended: false}), async function (req, res) {
+  const user = await usernameModel.create({username: req.body.username});
+  res.send({usename: user.username, _id: user._id});
+})
 
+app.post('/api/users/:_id/exercises', bodyParser.urlencoded({extended: false}), function (req, res) {
+  res.send('retrieveng exercises')
+})
 
-
+app.get('/api/users/:_id/logs/:from?/:to?/:limit?', function (req, res) {
+  console.log(req.params, req.query);
+  res.send(req.params);
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
