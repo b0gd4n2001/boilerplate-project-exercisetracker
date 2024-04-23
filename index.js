dd80a06987c2721223ea330956715073147d1083
b0gd4n2001 <lulelarub@gmail.com>
2024-04-23 05:37:41 +0300

    feat: added logs

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ mongoose.connect('mongodb+srv://bogdan:2Ff2NfVvK42ZbWw4@cluster0.qjyufua.mongodb
 
 const usernameSchema = new mongoose.Schema({ username: { type: String, required: true } });
 const usernameModel = mongoose.model('username', usernameSchema);
+const exerciseSchema = new mongoose.Schema(
+  {
+    userid: { type: String, required: true },
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: String, required: true }
+  }
+);
+const exerciseModel = mongoose.model('exercises', exerciseSchema);
 const logsSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
@@ -37,22 +47,56 @@ app.post('/api/users', bodyParser.urlencoded({ extended: false }), async functio
 })
 
 app.post('/api/users/:_id/exercises', bodyParser.urlencoded({ extended: false }), async function (req, res) {
-  let answer = {
-    description: req.body.description,
-    duration: Number(req.body.duration),
-    date: new Date(req.body.date) != 'Invalid Date' ? new Date(req.body.date).toDateString() : new Date().toDateString()
-  }
-  const userDoc = await logsModel.findById(req.params._id);
-  userDoc.log.push(answer);
-  await userDoc.save();
-  answer._id = userDoc._id;
-  answer.username = userDoc.username;
-  res.json(answer);
+  const username = await usernameModel.findById(req.params._id);
+  const exercise = await exerciseModel.create(
+    {
+      userid: req.params._id,
+      username: username.username,
+      date: new Date(req.body.date) != 'Invalid Date' ? new Date(req.body.date).toDateString() : new Date().toDateString(),
+      duration: Number(req.body.duration),
+      description: req.body.description
+    }
+  );
+  res.json(
+    {
+      _id: exercise.userid,
+      username: exercise.username,
+      date: exercise.date,
+      duration: exercise.duration,
+      description: exercise.description
+    }
+  );
 })
 
-app.get('/api/users/:_id/logs/:from?/:to?/:limit?', function (req, res) {
+app.get('/api/users/:_id/logs/:from?/:to?/:limit?', async function (req, res) {
   //console.log(req.params, req.query);
-  res.send(req.params);
+  const user = await usernameModel.findById(req.params._id);
+  const exercises = await exerciseModel.find({ userid: user._id });
+  let from = req.params.from ?? req.query.from ?? -Infinity;
+  let to = req.params.to ?? req.query.to ?? Infinity;
+  let limit = req.params.limit ?? req.query.limit ?? Infinity;
+  let log = new Array(exercises);
+  log = log.flat(Infinity);
+
+  log = log.filter((x) => Date.parse(x.date) >= from && Date.parse(x.date) <= to);
+
+  log = log.slice(0, limit);
+  log = log.map((x) => (
+    {
+      date: x.date,
+      duration: x.duration,
+      description: x.description
+    }
+  ));
+  console.log(log);
+  let answer = {
+    _id: user._id,
+    username: user.username,
+    count: log.length,
+    log: log
+  };
+
+  res.json(answer);
 })
 
 app.get('/api/users', async function (req, res) {
