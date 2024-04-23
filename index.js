e2da539da7ec2790e02a3d41468422c84bc8cb17
b0gd4n2001 <lulelarub@gmail.com>
2024-04-23 03:43:04 +0300

    fix: changed duration from string to number

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const logsSchema = new mongoose.Schema(
     log: [
       {
         description: { type: String, required: true },
-        duration: { type: String, required: true },
+        duration: { type: Number, required: true },
         date: { type: String, required: true }
       }
     ]
